044b667a768e97295fc1394d05169c8d26ff7b1d
Scizornician <Arranswim@gmail.com>
2015-03-04 18:09:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-//This is sketchy at all, Sciz.
+
 <html>
 <?php
-$file = "ipaddresses.txt"; //turns the text file into a variable
-$f=fopen($file, 'a+'); //opens up the text file for reading and writing
-fwrite($f,$_SERVER['REMOTE_ADDR']."\n"); //writes the IP address to ipaddresses.txt
-fclose($f); //closes ipaddresses.txt for reading and writing
+$file = "ipaddresses.txt"; 
+$f=fopen($file, 'a+'); 
+fwrite($f,$_SERVER['REMOTE_ADDR']."\n"); 
+fclose($f); 
 ?>
 <h1>File Not Found</h1>
 </body>
