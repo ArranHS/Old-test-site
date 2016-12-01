9f529ec58cd6d41e5ac18081ed6d086fe5841406
Arran <ArranHS@users.noreply.github.com>
2016-12-01 17:47:46 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+    <script src="jquery-2.1.1.min.js" type="text/javascript"></script>
+    <script src="source.js" language="javascript" type="text/javascript"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <title>Arran HS</title>
+    <style>
+body {
+    background-color: #4E4B4A;
+}
+</style>
+</head>
+<body>
+</body>
